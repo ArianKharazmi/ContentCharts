75d5c4e3ecd0a7e862b1423c06c24abbadc7de2f
Arian Kharazmi <100003892+ArianKharazmi@users.noreply.github.com>
2022-12-26 17:13:20 -0500

    ContentCharts v1.1.4 (minor patch)

--- main.py
+++ main.py
@@ -54,19 +54,19 @@ sidebar_selection = st.sidebar.radio(
 
 
 rss_urls = [
-    # Apple Music Most Played Songs
+    # Apple Music US Most Played Songs
     'https://rss.applemarketingtools.com/api/v2/us/music/most-played/100/songs.json'
-    # Apple Music Most Played Albums
+    # Apple Music US Most Played Albums
     'https://rss.applemarketingtools.com/api/v2/us/music/most-played/100/albums.json'
-    # Apple Music Most Played Playlists
+    # Apple Music US Most Played Playlists
     'https://rss.applemarketingtools.com/api/v2/us/music/most-played/100/playlists.json'
-    # Apple Music Most Played Music Videos
+    # Apple Music US Most Played Music Videos
     'https://rss.applemarketingtools.com/api/v2/us/music/most-played/100/music-videos.json'
-    # iTunes Store Most Actively Purchased Songs
+    # iTunes Store US Most Actively Purchased Songs
     'https://itunes.apple.com/us/rss/topsongs/limit=100/explicit=true/json'
-    # iTunes Store Most Actively Purchased Albums
+    # iTunes Store US Most Actively Purchased Albums
     'https://itunes.apple.com/us/rss/topalbums/limit=100/explicit=true/json'
-    # iTunes Store Most Actively Purchased Music Videos
+    # iTunes Store US Most Actively Purchased Music Videos
     'https://itunes.apple.com/us/rss/topmusicvideos/limit=100/explicit=true/json'
 
 
@@ -132,13 +132,13 @@ playedsongs, playedalbums, playedplaylists, playedmvs = get_data()
 
 
 
-print("-----Apple Music Charts (Top 100)-----")
+print("-----Apple Music US Charts (Top 100)-----")
 print("**Top songs actively played " + str(playedsongs))
 print("**Top albums actively played " + str(playedalbums))
 print("**Top playlists actively played " + str(playedplaylists))
 print("**Top Music Videos actively played " + str(playedmvs))
 
-st.header("Apple Music Charts (Top 100)")
+st.header("Apple Music US Charts (Top 100)")
 st.subheader("**Top songs actively played** " + str(playedsongs))
 st.subheader("**Top albums actively played** " + str(playedalbums))
 st.subheader("**Top playlists actively played** " + str(playedplaylists))
@@ -159,12 +159,12 @@ def get_data():
 
 
 topsongs, topalbums, topmvs = get_data()
-print("-----iTunes Store Music Charts (Top 100)-----")
+print("-----iTunes Store Music US Charts (Top 100)-----")
 print("**Top songs actively purchased " + str(topsongs))
 print("**Top albums actively purchased " + str(topalbums))
 print("**Top Music Videos actively purchased " + str(topmvs))
 
-st.header("iTunes Store Music Charts (Top 100)")
+st.header("iTunes Store Music US Charts (Top 100)")
 st.subheader("**Top songs actively purchased** " + str(topsongs))
 st.subheader("**Top albums actively purchased** " + str(topalbums))
 st.subheader("**Top Music Videos actively purchased** " + str(topmvs))
@@ -180,11 +180,11 @@ def get_data():
     return topfree, toppaid
 
 topfree, toppaid = get_data()
-print("-----App Store Charts (Top 100)-----")
+print("-----App Store US Charts (Top 100)-----")
 print("**Top free apps actively downloaded " + str(topfree))
 print("**Top paid apps actively purchased " + str(toppaid))
 
-st.header("App Store Charts (Top 100)")
+st.header("App Store US Charts (Top 100)")
 st.subheader("**Top free apps actively downloaded** " + str(topfree))
 st.subheader("**Top paid apps actively purchased** " + str(toppaid))
 
@@ -196,11 +196,11 @@ def get_data():
 
     return topmovies
 topmovies = get_data()
-print("-----iTunes Store Movies (AppleTV) Charts (Top 100)-----")
+print("-----iTunes Store Movies (AppleTV) US Charts (Top 100)-----")
 print("**Top movies actively purchased " + str(topmovies))
 
 
-st.header("iTunes Store Movies (AppleTV) Charts (Top 100)")
+st.header("iTunes Store Movies (AppleTV) US Charts (Top 100)")
 st.subheader("**Top movies actively purchased** " + str(topmovies))
 
 
@@ -217,12 +217,12 @@ def get_data():
     return toppodcasts, toppodeps, toppodsubs
 
 toppodcasts, toppodeps, toppodsubs = get_data()
-print("-----Apple Podcasts Charts (Top 100)-----")
+print("-----Apple Podcasts US Charts (Top 100)-----")
 print("**Top podcasts actively listened to " + str(toppodcasts))
 print("**Top podcast episodes actively listened to " + str(toppodeps))
 print("**Top podcasts actively subscribed to " + str(toppodsubs))
 
-st.header("Apple Podcasts Charts (Top 100)")
+st.header("Apple Podcasts US Charts (Top 100)")
 st.subheader("**Top podcasts actively listened to** " + str(toppodcasts))
 st.subheader("Top podcast episodes actively listened to " + str(toppodeps))
 st.subheader("Top podcasts actively subscribed to " + str(toppodsubs))
