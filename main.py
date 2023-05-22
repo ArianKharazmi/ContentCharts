19d4d08a0255a857cc23a6f0ae0c00f388584c9e
ariankharazmi <100003892+ariankharazmi@users.noreply.github.com>
2023-05-22 14:27:08 -0400

    Code commit for Pandas 2.0 release

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import streamlit as st
 import base64
 import json
 import pandas as pd
-from pandas.io.json import json_normalize
+from pandas.json_normalize
 #import feedparser
 import xml.etree.ElementTree as ET
 
